f629530e3fb7f8fa30f1ee1cfa5d6759f3aff8ad
nblake707 <33844526+nblake707@users.noreply.github.com>
2024-03-29 21:18:03 -0400

    Refractored route handlers added validation

--- index.js
+++ index.js
@@ -1,89 +1,113 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+require("dotenv").config(); // should be imported before the Note model. Env vars available globally
+const express = require("express");
+const cors = require("cors");
+const Note = require("./models/note");
 
-// middleware parses incoming requests with JSON payloads
-app.use(express.json())
-
-// middleware enables CORS
-app.use(cors())
+const app = express();
 
 // middleware used to show static content
 // Express will first check here upon receiving a request
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
-let notes = [
-  {
-    id: 1,
-    content: "Take out the trash.",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Walk the dog.",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "Empty the dishwasher.",
-    important: true,
-  },
-];
+// middleware enables CORS
+app.use(cors());
+
+// middleware parses incoming requests with JSON payloads - must come before handler functions
+app.use(express.json());
+app.use(requestLogger);
 
 /*  body property would be undefined without the json-parser
     json-parser takes the JSON data of a request, transforms it into 
     a Javascript object and then attaches it to the body property of the request
     object before the route handler is called. 
 */
-
-const generateId = () => {
-  const maxId =
-    notes.length > 0
-      ? Math.max(...notes.map((n) => n.id)) // math.max returns largest #. spread used to provide list of args
-      : 0;
-
-  return maxId;
-}
-
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
+      // Bad Request - server unable to fulfill request
       error: "content missing",
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: Boolean(body.important) || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
+});
 
-  response.json(note);
-})
-
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  // Objects = truthy & undefined = falsy
-  note ? response.json(note) : response.status(404).end();
-})
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => (note ? response.json(note) : response.status(404).end()))
+    .catch((error) => next(error));
+});
 
 app.get("/api/notes", (req, res) => {
-  res.json(notes);
-})
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
+});
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
-  response.status(204).end();
-})
+app.put("api/notes/:id", (request, response, next) => {
+  const { content, important } = request.body;
 
-const PORT = process.env.PORT || 3001;
+  /*
+     There is one important detail regarding the use of the findByIdAndUpdate 
+     method. By default, the updatedNote parameter of the event handler receives 
+     the original document without the modifications. We added the optional 
+     { new: true } parameter, which will cause our event handler to be called 
+     with the new modified document instead of the original.
+  */
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+// why are the params diff for each of the error handlers
+const unknownEndpoint = (request, response) => {
+  // needs to be right before the errorHandler / after HTTP request handler
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint); // handles requests with an unknown endpoint
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name == "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+// must be the last loaded middleware, all the routes should be registered before this.
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
